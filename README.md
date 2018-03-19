e32a2b27b16ab3b271c4493a0cc19795a6c06e7b
zgdong <flash8627@hotmail.com>
2018-03-19 23:15:58 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# docker-hello-world
+
+
+1.mvn clean package docker:build -e -DskipTests
+
+[root@localhost docker-helloworld]# docker images
+REPOSITORY                            TAG                 IMAGE ID            CREATED             SIZE
+flash8627/docker-spring-boot          latest              17b6aab0f77a        5 minutes ago       181MB
+
+2.docker run -p 8080:8080 -t flash8627/docker-spring-boot
+
+3.http://docker-server-ip:8080/
+
+
+
---
+++ src/main/java/com/gwtjs/docker/Application.java
@@ -0,0 +1,27 @@
+package com.gwtjs.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
+/**
+ * 主应用入口
+ * @author <a href="http://gwtjs.com">gwtjs.com</a>
+ * @date 2016年3月19日
+ */
+@SpringBootApplication
+@RestController
+public class Application {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World. <br />"
+				+ "Welcome to <a href='http://gwtjs.com'>gwtjs.com</a></li>";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
